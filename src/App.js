d8f371fb0b5966cd0d6cb09916541e6a892575df
Long <dangtienlong2020@gmail.com>
2022-05-17 23:48:19 +0700

    them sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import './App.css';
-import { Router, Switch } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import "antd/dist/antd.css";
 import {HomeTemplate} from './templates/HomeTemplate/HomeTemplate';
 import { Admintemplate } from './templates/Admintemplate/Admintemplate';
 import Home from './pages/Home/Home';
 import Admin from './pages/Admin/Admin';
-
+import Course from './components/Course/Course';
 export const history = createBrowserHistory();
 
 function App() {
   return (
     <Router history={history}>
       <div className="App">
-        <Switch>
+        
 
           <HomeTemplate path='/' component={Home}/>
+          <HomeTemplate path='/course' component={Course}/>
+
           <Admintemplate path='/admin' component={Admin}/>
           
-        </Switch>
+        
         
       </div>
     </Router>
---
+++ src/components/Course/Course.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+
+export default function Course(props) {
+
+  useEffect(()=>{
+    window.scrollTo(0,0)
+})
+
+
+  return (
+    <div>Course</div>
+  )
+}
