33423575413e30a45b7fbb7a5cd6b034feab4fe0
Quoc Ky <huynhquocky97@gmail.com>
2022-06-11 20:17:58 +0700

    backup code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,84 +1,127 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import Login from '../Form/Login';
-import { Button, Modal } from 'antd';
+import { Badge, Modal, Drawer } from 'antd';
 import Register from '../Form/Register';
 import styled from 'styled-components';
 import LogOut from '../Form/Logout';
-
-
+import { ShoppingCartOutlined } from '@ant-design/icons';
 
 export default function Header(props) {
-  const dispatch = useDispatch()
-  const { Component, isVisible } = useSelector(state => state.ModalReducer)
-  const { userLogin } = useSelector(state => state.QuanLyNguoiDungReducer)
-  const [title, setTitle] = useState("");
+  const dispatch = useDispatch();
+  const { Component, isVisible } = useSelector((state) => state.ModalReducer);
+  const { userLogin } = useSelector((state) => state.QuanLyNguoiDungReducer);
+  const [title, setTitle] = useState('');
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
 
   const handleCancel = () => {
     dispatch({
       type: 'CLOSE_MODAL',
       isVisible: false,
-    })
+    });
   };
 
   const showLogin = () => {
-    setTitle("Login");
+    setTitle('Login');
     dispatch({
       type: 'OPEN_MODAL',
       Component: <Login />,
       isVisible: true,
-    })
+    });
   };
 
   const showRegister = () => {
-    setTitle("Register");
+    setTitle('Register');
     dispatch({
       type: 'OPEN_MODAL',
       Component: <Register />,
       isVisible: true,
-    })
-  };
-
-  const goTop = () => {
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
     });
   };
+
+  // const goTop = () => {
+  //   window.scroll({
+  //     top: 0,
+  //     left: 0,
+  //     behavior: 'smooth',
+  //   });
+  // };
   return (
-    <header className=''>
-      <nav className="container navbar navbar-expand-sm navbar-light bg-light">
-        <NavLink className="navbar-brand" to="/">Elearning</NavLink>
-        <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+    <HeaderWrapper className="navbar-light bg-light">
+      <nav className="wrapper-container navbar navbar-expand-sm">
+        <NavLink className="navbar-brand" to="/">
+          Elearning
+        </NavLink>
+        <button
+          className="navbar-toggler d-lg-none"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsibleNavId"
+          aria-controls="collapsibleNavId"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="collapsibleNavId">
           <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
             <li className="nav-item active">
-              <NavLink className="nav-link" to="/">HOME</NavLink>
-            </li><li className="nav-item">
-              <NavLink className="nav-link" to="/">COURSE</NavLink>
+              <NavLink className="nav-link" to="/">
+                HOME
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">
+                COURSE
+              </NavLink>
             </li>
           </ul>
-          <div className="navbar-right ml-auto my-2 my-lg-0" >
-            {!!userLogin.taiKhoan ?
-              <LogOut />
-              :
+          <div className="navbar-right ml-auto my-2 my-lg-0">
+            {!!userLogin.taiKhoan ? (
+              <div className="d-flex align-items-center">
+                <LogOut />
+                <Badge count={5} className="ml-10">
+                  <ShoppingCartOutlined onClick={showDrawer} />
+                </Badge>
+                <Drawer
+                  title="Basic Drawer"
+                  placement="right"
+                  onClose={onClose}
+                  visible={visible}
+                  width={500}
+                >
+                  <p>Some contents...</p>
+                  <p>Some contents...</p>
+                  <p>Some contents...</p>
+                </Drawer>
+              </div>
+            ) : (
               <>
-                <button type="button" className="btn btn-primary"
+                <button
+                  type="button"
+                  className="btn btn-primary"
                   onClick={showLogin}
                 >
                   Log in
                 </button>
 
-                <button className="btn btn-outline-success my-2 my-sm-0 ml-2"
+                <button
+                  className="btn btn-outline-success my-2 my-sm-0 ml-2"
                   onClick={showRegister}
                 >
                   Register
                 </button>
-              </>}
+              </>
+            )}
           </div>
         </div>
       </nav>
@@ -91,14 +134,37 @@ export default function Header(props) {
       >
         {Component}
       </ModalWrapper>
-
-    </header>
-  )
+    </HeaderWrapper>
+  );
 }
 
 const ModalWrapper = styled(Modal)`
   .ant-modal {
     top: 50px !important;
   }
-`
+`;
 
+const HeaderWrapper = styled.header`
+  width: 100%;
+  padding: 10px 0;
+
+  .wrapper-container {
+    width: 90%;
+    margin: auto;
+  }
+
+  .navbar-brand {
+    font-size: 24px;
+    font-weight: 700;
+    color: #3498db;
+  }
+
+  .ml-10 {
+    margin-left: 10px;
+  }
+
+  .anticon-shopping-cart {
+    font-size: 28px;
+    cursor: pointer;
+  }
+`;
